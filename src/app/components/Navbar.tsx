3b3d845c110568df67769e2522070943535f364e
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2024-12-08 02:43:50 -0300

    feat: cambios en navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -7,40 +7,58 @@ import Link from "next/link";
 
 const Navbar: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const menuRef = useRef<HTMLDivElement>(null); //Referencia al contenedor del menu
+  const menuRef = useRef<HTMLDivElement>(null); // Referencia al contenedor del menú
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  //detectar click fuera del menu
+  // Detectar clic fuera del menú
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-        setIsMenuOpen(false); //cerrar el menu
+        setIsMenuOpen(false); // Cerrar el menú
       }
     };
-    //agregar evento global de click
     document.addEventListener("mousedown", handleClickOutside);
-
-    //limpiar el evento cuando el componente se desmoenta
 
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
+
   return (
     <nav className="fixed w-full z-50 bg-gray-900/70 backdrop-blur-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          {/* LOGO  */}
+          {/* LOGO */}
           <div className="flex items-center">
             <Image
-              src="/logo.jpg"
-              alt="Bodega Don Milagro"
+              src="/logo.jpg" // Cambiar a la imagen del logo de Mila Rock
+              alt="Mila Rock - Milanesas Argentinas"
               width={120}
               height={30}
               className="h-12 w-auto"
+            />
+          </div>
+
+          {/* Banderas */}
+          <div className="hidden md:flex items-center space-x-2">
+            {/* Bandera de Argentina */}
+            <Image
+              src="/flags/arg.flag.png" // Asegúrate de agregar la imagen en la carpeta public
+              alt="Bandera de Argentina"
+              width={34}
+              height={26}
+              className="w-8 h-6"
+            />
+            {/* Bandera de Bolivia */}
+            <Image
+              src="/flags/bolivia.flag.png" // Asegúrate de agregar la imagen en la carpeta public
+              alt="Bandera de Bolivia"
+              width={24}
+              height={16}
+              className="w-8 h-6"
             />
           </div>
 
@@ -48,7 +66,7 @@ const Navbar: React.FC = () => {
           <div className="flex items-center justify-between h-16">
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
-                {["Inicio", "Vinos", "Nosotros", "Ubicación", "Contacto"].map(
+                {["Inicio", "Menú", "Nosotros", "Ubicación", "Contacto"].map(
                   (item) => (
                     <Link
                       key={item}
@@ -63,7 +81,7 @@ const Navbar: React.FC = () => {
             </div>
           </div>
 
-          {/* mobile  */}
+          {/* Mobile */}
           <div className="md:hidden">
             <button
               type="button"
@@ -76,13 +94,13 @@ const Navbar: React.FC = () => {
               ref={menuRef}
               className={`md:hidden fixed top-16 right-0 bg-gray-900 w-full z-40 transform transition-all duration-300 ease-out ${
                 isMenuOpen
-                  ? "bopacity-100 translate-y-0"
+                  ? "opacity-100 translate-y-0"
                   : "opacity-0 -translate-y-10 pointer-events-none"
               }`}
             >
-              <div className="flex flex-col items-center text-center  justify-center h-full">
-                <div className=" flex flex-col space-y-4 w-full">
-                  {["Inicio", "Vinos", "Nosotros", "Ubicación", "Contacto"].map(
+              <div className="flex flex-col items-center text-center justify-center h-full">
+                <div className="flex flex-col space-y-4 w-full">
+                  {["Inicio", "Menú", "Nosotros", "Ubicación", "Contacto"].map(
                     (item) => (
                       <Link
                         key={item}
